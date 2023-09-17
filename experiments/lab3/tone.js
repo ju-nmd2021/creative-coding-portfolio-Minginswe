3a89fa8ff0aa025a2330f3dd30a33f25ca71029c
Minginswe <xiaomingvlog@gmail.com>
2023-09-17 22:08:49 +0200

    Update tone.js

--- experiments/lab3/tone.js
+++ experiments/lab3/tone.js
@@ -1,7 +1,73 @@
-let cNote = document.getElementsByClassName("c");
+let c = document.getElementById("c");
+let cSharp = document.getElementById('cSharp');
+let d = document.getElementById('d');
+let dSharp = document.getElementById('dSharp');
+let e = document.getElementById('e');
+let f = document.getElementById('f');
+let fSharp = document.getElementById('fSharp');
+let g = document.getElementById('g');
+let gSharp = document.getElementById('gSharp');
+let a = document.getElementById('a');
+let aSharp = document.getElementById('aSharp');
+let b = document.getElementById('b')
 
-cNote[0].addEventListener("click", () => {
+
+
+c.addEventListener("click", () => {
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease("C4", "8n");
-    console.log('c')
 });
+
+cSharp.addEventListener("click", () => {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("C#4", "8n");
+});
+
+d.addEventListener("click", () => {
+        const synth = new Tone.Synth().toDestination();
+        synth.triggerAttackRelease("D4", "8n");
+})
+
+dSharp.addEventListener("click", () => {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("D#4", "8n");
+})
+
+e.addEventListener("click", () => {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("E4", "8n");
+})
+
+f.addEventListener("click", () => {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("F4", "8n");
+})
+
+fSharp.addEventListener("click", () => {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("F#4", "8n");
+})
+
+g.addEventListener("click", () => {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("G4", "8n");
+})
+
+gSharp.addEventListener("click", () => {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("G#4", "8n");
+})
+
+a.addEventListener("click", () => {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("A4", "8n");
+})
+
+aSharp.addEventListener("click", () => {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("A#4", "8n");
+})
+b.addEventListener("click", () => {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("B4", "8n");
+})
